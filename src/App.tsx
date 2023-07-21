fa77f575d5099ed3c32e100d354c09f074a2049d
amiransht <amira19nisrina@gmail.com>
2023-07-21 20:15:34 +0700

    setting route

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { Route, Routes } from "react-router-dom";
+import Intro from "./pages/Auth/Intro/Intro";
+import Register from "./pages/Auth/Register/Register";
+import Home from "./pages/Home/Home";
+import CreateUsername from "./pages/Auth/Register/CreateUsername";
+import Login from "./pages/Auth/Login/Login";
+import ForgotPassword from "./pages/Auth/ForgotPassword/ForgotPassword";
+import EmailSent from "./pages/Auth/ForgotPassword/EmailSent";
+import ResetPassword from "./pages/Auth/ForgotPassword/ResetPassword";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/register/create" element={<CreateUsername/>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/login/forgot" element={<ForgotPassword />} />
+          <Route path="/login/forgot/sent" element={<EmailSent />} />
+          <Route path="/reset" element={<ResetPassword />} />
+        <Route path="/home" element={<Home />} />
+        </Routes>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
