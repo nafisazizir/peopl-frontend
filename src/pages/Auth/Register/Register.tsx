f4fff4d9a0c580bb412694715151f8768a1512d8
Nafis Azizi Riza <101693218+nafisazizir@users.noreply.github.com>
2023-07-22 02:10:42 +0700

    refactor: delete unused import

--- src/pages/Auth/Register/Register.tsx
+++ src/pages/Auth/Register/Register.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, useState, useEffect } from "react";
+import React, { ChangeEvent, useState } from "react";
 import "./RegisterStyle.css";
 import Label from "../../../components/Label/Label";
 import { AxiosError } from "axios";
