ed8f7036101d0e4ee936ef7a5e07f061d70d613d
syafiqo.ararashy <syafiqo.arashy@ui.ac.id>
2023-07-22 22:19:33 +0700

    matchmaking service progress

--- src/App.tsx
+++ src/App.tsx
@@ -28,9 +28,12 @@ function App() {
         <Route path="/login/forgot/sent" element={<EmailSent />} />
         <Route path="/reset" element={<ResetPassword />} />
         <Route path="/home" element={<Home />} />
-        <Route path="/search/posts" element={<Search />} />
+        <Route path="/search" element={<Search />} />
         <Route path="/create" element={<CreatePost />} />
-
+        <Route path="/profile/:username" element={<Profile />} />
+        <Route path="/community/:communityName" element={<Community />} />
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/matchmaking" element={<Matchmaking />} />
       </Routes>
     </>
   );
