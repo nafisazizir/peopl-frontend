bd7f11bd5656ac24c120ac29cd915c8b7e4715c7
syafiqo.ararashy <syafiqo.arashy@ui.ac.id>
2023-07-22 15:17:49 +0700

    desktop navbar done

--- src/components/Button/HeaderButton/HeaderButton.tsx
+++ src/components/Button/HeaderButton/HeaderButton.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import "./HeaderButtonStyle.css";
+import "./NotifButtonStyle.css";
 
-interface ButtonHeaderProps {
+interface ButtonNotifProps {
   onClick: (event: React.MouseEvent<HTMLDivElement>) => void;
   buttonText: string
 }
 
-const HeaderButton: React.FC<ButtonHeaderProps> = ({ buttonText, onClick }) => {
+const HeaderButton: React.FC<ButtonNotifProps> = ({ buttonText, onClick }) => {
   return (
     <div className={"button-header"} onClick={onClick}>
       <div className="button-text">{buttonText}</div>
